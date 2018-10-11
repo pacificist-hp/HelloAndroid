5733d7c4bad0cc26ff90c0ed21cbf0efd8d768c7
pacificist <pacificist@outlook.com>
2018-10-11 22:49:23 +0800

    add a function

--- app/src/main/java/com/android/pacificist/helloandroid/MainActivity.kt
+++ app/src/main/java/com/android/pacificist/helloandroid/MainActivity.kt
@@ -2,11 +2,17 @@ package com.android.pacificist.helloandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("heping", "3 + 4 = " + sum(3, 4))
+    }
+
+    private fun sum(a: Int, b: Int): Int {
+        return a + b
     }
 }
