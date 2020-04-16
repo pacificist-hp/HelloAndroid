de06b616662b265f7477db6235d70d1d88a36aba
pacificist <pacificist@outlook.com>
2020-04-16 21:47:03 +0800

    rollback app

--- app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
+++ app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
@@ -2,28 +2,25 @@ package com.android.pacificist.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+
+import com.android.pacificist.bridge.Bridge;
+import com.android.pacificist.bridge.Evaluator;
+import com.android.pacificist.bridge.Value;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Bridge.instance().registerFunction("log", 1, new Bridge.Callable() {
+            @Override
+            public Value call(Evaluator evaluator, Value[] params) {
+                Log.d("JavaApplication", params[0].toString());
+                return null;
+            }
+        });
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
