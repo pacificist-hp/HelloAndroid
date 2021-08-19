68dc4aacb68d8d35f6351a8464cc2cd67529412d
pacificist <pacificist@outlook.com>
2021-08-19 23:35:46 +0800

    init siddhi demo with errors

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# HelloAndroid
-some debug code of Android
+# siddhi demo on Android
+
+[siddhi 官网](https://siddhi.io/)
+
+[siddhi-android-platform](https://github.com/siddhi-io/siddhi-android-platform)
+
+[siddhi-android-platform-quickstart](https://github.com/siddhi-io/siddhi-android-platform/blob/master/docs/documentation/siddhi-android-platform-quickstart.md)
--- app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
+++ app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
@@ -1,13 +1,54 @@
 package com.android.pacificist.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String app = "@app:name('foo')@source(type='android-humidity', @map(type='keyvalue'," +
+            "fail.on.missing.attribute='false',@attributes(sensor='sensor',vector='humidity')))" +
+            "define stream sensorInStream ( sensor string, vector float);" +
+            "@sink(type='android-notification' , title='Details',multiple.notifications = 'true'," +
+            " @map(type='keyvalue'))define stream outputStream (sensor string, vector float); " +
+            "from sensorInStream select * insert into outputStream";
+
+    private String appName;
+
+    private SiddhiAppController appController;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            appController = SiddhiAppController.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            appController = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, SiddhiAppService.class);
+        startService(intent);
+        bindService(intent,serviceConnection,BIND_AUTO_CREATE);
+    }
+
+    public void startApp(View view) throws RemoteException {
+        appName = appController.startSiddhiApp(app);
+    }
+    public void stopApp(View view) throws RemoteException{
+        appController.stopSiddhiApp(appName);
     }
 }
