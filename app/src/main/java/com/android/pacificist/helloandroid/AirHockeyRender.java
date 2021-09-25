dd04f968c059ec914c2107a4fd3df82cd7e290d0
pacificist <pacificist@outlook.com>
2021-09-25 23:58:41 +0800

    add GLSurfaceView

---
+++ app/src/main/java/com/android/pacificist/helloandroid/AirHockeyRender.java
@@ -0,0 +1,26 @@
+package com.android.pacificist.helloandroid;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.*;
+
+class AirHockeyRender implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(0.0f, 0.7f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
--- app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
+++ app/src/main/java/com/android/pacificist/helloandroid/MainActivity.java
@@ -1,13 +1,36 @@
 package com.android.pacificist.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView glSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+//        glSurfaceView = findViewById(R.id.id_surface_view);
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new AirHockeyRender());
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
     }
 }
