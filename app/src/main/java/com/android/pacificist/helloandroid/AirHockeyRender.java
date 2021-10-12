89a2781b3e3a45778fecf93a1812a2535761a6d5
pacificist <pacificist@outlook.com>
2021-10-12 23:40:40 +0800

    3D: add w

--- app/src/main/java/com/android/pacificist/helloandroid/AirHockeyRender.java
+++ app/src/main/java/com/android/pacificist/helloandroid/AirHockeyRender.java
@@ -22,7 +22,7 @@ class AirHockeyRender implements GLSurfaceView.Renderer {
     private static final String A_POSITION = "a_Position";
     private static final String A_COLOR = "a_Color";
 
-    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int POSITION_COMPONENT_COUNT = 4;
     private static final int COLOR_COMPONENT_COUNT = 3;
 
     private static final int BYTES_FLOAT = 4;
@@ -43,23 +43,25 @@ class AirHockeyRender implements GLSurfaceView.Renderer {
 
     public AirHockeyRender(Context context) {
         float[] tableVerticesWithTriangles = {
+                // Order of coordinates: X, Y, Z, W, R, G, B
+
                 // Triangle Fan
-                0f, 0f, 1f, 1f, 1f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0f, 0f, 0f, 1.5f, 1f, 1f, 1f,
+                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
 
                 // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 0f, 0f, 1f,
+                -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+                0.5f, 0f, 0f, 1.5f, 0f, 0f, 1f,
 
                 // Mallet 1
-                0f, -0.4f, 0f, 0f, 1f,
+                0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
 
                 // Mallet 2
-                0f, 0.4f, 1f, 0f, 0f
+                0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
